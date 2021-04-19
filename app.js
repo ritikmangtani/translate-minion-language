7a71fa4a5a4f0eddbb07fffb09e9362ffa689bbe
ritikmangtani <mangtaniritik@gmail.com>
2021-04-19 15:38:26 +0530

    feat, style : text translates to banana language

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-var userName = prompt("Give me username");
-var welcomeMessage = "tthis script works" + userName;
-alert(welcomeMessage)
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector('#txt-area')
+var outputDiv = document.querySelector('#output')
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    alert("Something went wrong! Try again later.")
+}
+
+function clickhandler(){
+    var inputTxt = txtInput.value // taking input
+    fetch(getTranslationURL(inputTxt))
+    .then(response => response.json())
+    .then(json =>{
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText; //output
+    })
+    .catch(errorHandler)
+}
+btnTranslate.addEventListener("click", clickhandler)
